ab7f5f2a75216cf129412b9acaaf45cc106dbfce
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-12 08:48:13 +0530

    Done

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,11 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = ((x2-x1)**2 + (y2-y1)**2)
+	b = ((x3-x2)**2 + (y3-y2)**2)
+	c = ((x3-x1)**2 + (y3-y1)**2)
+	if a == b+c or b == a+c or c == a+b:
+		return True
+	return False
+		
+print(isrighttriangle(-1, 7, 10, -4, 12, -2))
