04eae416facf0e5a50abe7a65cca3b9f31aa7294
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-17 18:31:12 +0530

    Done

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -4,7 +4,21 @@
 # 196-algorithm, after the most famous number associated with the process.
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
+def lychrel(n):
+	for i in range(50):
+		n = n+int(str(n)[::-1])
+		if n == int(str(n)[::-1]):
+			return False
+	return True
 
 def nthlychrelnumbers(n):
 	# your code goes here
-	pass
+	count = 1
+	num = 197
+	if n == 1:
+		return 196
+	while count < n:
+		if lychrel(num):
+			count+=1
+		num+=1
+	return num-1
