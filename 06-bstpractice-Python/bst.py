ce78d640af0dcf539a6c87aa7ffa6f0e18e7b707
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-21 10:29:45 +0530

    Done

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,45 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        node = Node(new_val)
+        if(self.root):
+            current = self.root
+            parent = None
+            while(current !=None):
+                parent = current
+                if(new_val < current.value):
+                    current = current.left
+                else:
+                    current = current.right
+            if(new_val < parent.value):
+                parent.left = node
+            else:
+                parent.right = node
+        else:
+            self.root = node
+        return self.root
 
     def printSelf(self):
         # Your code goes here
-        pass
+        print( self.root.value )
+        return self.root
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        while self.root != None:
+            if self.root.value == find_val:
+                return True
+            elif self.root.value < find_val:
+                self.root = self.root.right
+            elif self.root.value > find_val:
+                self.root = self.root.left
+        return False
 
+tree = BST(4)
+
+tree.printSelf().value
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+print(tree.search(-1))
