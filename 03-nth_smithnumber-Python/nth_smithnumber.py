9d6df0c85286ea921b10004df41d3fca4e10a632
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-25 15:31:06 +0530

    Done

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,5 +8,63 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+ 
 def fun_nth_smithnumber(n):
-    return 1
+   count=0
+   x=1
+   y = 0
+   while(n+1!=count):
+       if(isSmith(x)):
+           count+=1
+           y = x
+       x+=1
+   return y
+ 
+def isPrime(n):
+   if (n < 2):
+       return False
+   if (n == 2):
+       return True
+   if (n % 2 == 0):
+       return False
+   for i in range(3,n//2+1,2):
+       if (n % i == 0):
+           return False
+   return True
+ 
+def isSmith(n):
+   if(isPrime(n)!=True):
+       val = n
+       lis = []
+       factors_sum=0
+       res = 0
+       if n==1:
+           return False
+       for i in range(2,(n//2) + 1):
+           if(isPrime(i)==True) and (n%i==0):
+               lis.append(i)
+          
+       for an in str(n):
+           res+=int(an)
+      
+       li=[]
+       for i in lis:
+           while (val%i==0 and val!=0):
+               li.append(i)   
+               factors_sum+=i
+               val=val//i
+      
+       val_final = 0       
+       for j in li:
+           if(j>9):
+               for d in str(j):
+                   val_final = val_final+int(d)
+           else:
+               val_final = val_final+j
+      
+       if(res==val_final):
+           return True
+       else:
+           return False
+
+print(isSmith(12))
