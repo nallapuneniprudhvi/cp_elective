8795160e17d9cd7b3185e93acab205ab32387a57
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-27 17:00:11 +0530

    Done

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -9,4 +9,16 @@
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    start = 0
+    end = len(s2)
+    l = [[0 for i in range(len(s2)+1)] for j in range(len(s1) + 1)]
+    for i in range(1,len(s1)+1):
+        for j in range(1,len(s2)+1): 
+            if s1[i-1] == s2[j-1]:
+                print(s1[i-1])
+                print("i,j",l[i-1][j-1]+1)
+                l[i][j] = l[i-1][j-1] + 1
+                if l[i][j] >= start:
+                    start = l[i][j]
+                    end = i
+    return s1[end - start: end]
