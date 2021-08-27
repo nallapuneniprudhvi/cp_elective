7e03c88285925da79824cd4d997485051efd938d
nallapuneniprudhvi <72134872+nallapuneniprudhvi@users.noreply.github.com>
2021-08-27 17:28:51 +0530

    Done

--- 08-getallpermutations-Python/getallpermutations.py
+++ 08-getallpermutations-Python/getallpermutations.py
@@ -3,5 +3,21 @@
 # your program should print all 6 permutations e.g. [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
 
 def getallpermutations(x):
-	# Your code goes here
-	pass
+	stri = ""
+	return permutation(x,stri,l1=[])
+
+
+def permutation(res,ans,l1):
+	if len(res) == 0:
+		l2 = []
+		for i in ans:
+			l2.append(i)
+		t = tuple(l2)
+		l1.append(t)
+	for i in range(len(res)):
+		ch = res[i]
+		left = res[0:i]
+		right = res[i+1:]
+		resu = left + right
+		permutation(resu,ans+ch,l1)
+	return l1
